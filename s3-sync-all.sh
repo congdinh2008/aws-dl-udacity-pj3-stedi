ff9d15523243adcfc34e55411346052f42a01912
Cong Dinh <congdinh2012@hotmail.com>
2023-04-04 14:28:09 +0700

    Add gitignore and config file

--- s3-sync-all.sh
+++ s3-sync-all.sh
@@ -1,3 +1,3 @@
-aws s3 sync ./project/customers/ s3://yourname-stedi-lakehouse/customer/landing
-aws s3 sync ./project/accelerometer/ s3://yourname-stedi-lakehouse/accelerometer/landing
-aws s3 sync ./project/step_trainer/ s3://yourname-stedi-lakehouse/step_trainer/landing
+aws s3 sync ./data/customers/ s3://congdinh2023-stedi-lakehouse/customer/landing
+aws s3 sync ./data/accelerometer/ s3://congdinh2023-stedi-lakehouse/accelerometer/landing
+aws s3 sync ./data/step_trainer/ s3://congdinh2023-stedi-lakehouse/step_trainer/landing
